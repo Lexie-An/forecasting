22307ca34e5e0e681f858bc8a3d366811938b601
Lexie_An <62637512+Lexie-An@users.noreply.github.com>
2023-08-26 02:14:19 +0800

    Add files via upload

---
+++ Prototype/main.js
@@ -0,0 +1,15 @@
+function openForgotpass() {
+    document.getElementById("forgotpass").style.display = "block";
+  }
+  
+  function closeForgotpass() {
+    document.getElementById("forgotpass").style.display = "none";
+  }
+
+  function openResetpass() {
+    document.getElementById("resetpass").style.display = "block";
+  }
+  
+  function closeResetpass() {
+    document.getElementById("resetpass").style.display = "none";
+  }
