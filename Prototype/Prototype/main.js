f4b85f1c199f7b4a9bcc279f0be15ecb1688f5b7
Lexie_An <62637512+Lexie-An@users.noreply.github.com>
2023-08-28 01:00:55 +0800

    Add files via upload

---
+++ Prototype/Prototype/main.js
@@ -0,0 +1,55 @@
+function openForgotpass() {
+    document.getElementById("forgotpass").style.display = "block";
+  }
+  
+  function closeForgotpass() {
+    document.getElementById("forgotpass").style.display = "none";
+  }
+
+  function openResetpass() {
+    document.getElementById("resetpass").style.display = "block";
+  }
+  
+  function closeResetpass() {
+    document.getElementById("resetpass").style.display = "none";
+  }
+
+  function openaddproduct() {
+    document.getElementById("addproduct").style.display = "block";
+  }
+  
+  function closeaddproduct() {
+    document.getElementById("addproduct").style.display = "none";
+  }
+
+  function openaddcategory() {
+    document.getElementById("addcategory").style.display = "block";
+  }
+  
+  function closeaddcategory() {
+    document.getElementById("addcategory").style.display = "none";
+  }
+
+  function openaddbrand() {
+    document.getElementById("addbrand").style.display = "block";
+  }
+  
+  function closeaddbrand() {
+    document.getElementById("addbrand").style.display = "none";
+  }
+
+  function openaddsupplier() {
+    document.getElementById("addsupplier").style.display = "block";
+  }
+  
+  function closeaddsupplier() {
+    document.getElementById("addsupplier").style.display = "none";
+  }
+
+  function openadduseracc() {
+    document.getElementById("adduseracc").style.display = "block";
+  }
+  
+  function closeadduseracc() {
+    document.getElementById("adduseracc").style.display = "none";
+  }
