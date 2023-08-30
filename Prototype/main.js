f3c3e8325e122579259447598948c46f03cb4ac0
Lexie_An <62637512+Lexie-An@users.noreply.github.com>
2023-08-30 18:25:27 +0800

    Add files via upload

--- Prototype/main.js
+++ Prototype/main.js
@@ -13,3 +13,43 @@ function openForgotpass() {
   function closeResetpass() {
     document.getElementById("resetpass").style.display = "none";
   }
+
+  function openaddproduct() {
+    document.getElementById("addproduct").style.display = "block";
+  }
+  
+  function closeaddproduct() {
+    document.getElementById("addproduct").style.display = "none";
+  }
+
+  function openaddcategory() {
+    document.getElementById("addcategory").style.display = "block";
+  }
+  
+  function closeaddcategory() {
+    document.getElementById("addcategory").style.display = "none";
+  }
+
+  function openaddbrand() {
+    document.getElementById("addbrand").style.display = "block";
+  }
+  
+  function closeaddbrand() {
+    document.getElementById("addbrand").style.display = "none";
+  }
+
+  function openaddsupplier() {
+    document.getElementById("addsupplier").style.display = "block";
+  }
+  
+  function closeaddsupplier() {
+    document.getElementById("addsupplier").style.display = "none";
+  }
+
+  function openadduseracc() {
+    document.getElementById("adduseracc").style.display = "block";
+  }
+  
+  function closeadduseracc() {
+    document.getElementById("adduseracc").style.display = "none";
+  }
